7d4efb7fc3226b9ff545cb58270dd5a41b4077d1
ducpt <ducpt1012@gmail.com>
2021-11-21 22:13:55 +0700

    Implement by ruby

---
+++ client.rb
@@ -0,0 +1,9 @@
+require 'grpc'
+
+# stub = StudentsDetails::StudentsDetailsService::Stub.new(
+#  '0.0.0.0:50052', :this_channel_is_insecure
+# )
+ 
+# request = StudentsDetails::HelloRequest.new(name: "Harry")
+# response = stub.hello(request)
+# puts response.body
